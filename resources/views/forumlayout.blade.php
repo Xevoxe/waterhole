947a81e1f40684995cc978b954beaf94bd6a6887
John Doe <xevoxe@gmail.com>
2019-07-15 12:14:30 -0500

    Initial Commit

---
+++ resources/views/forumlayout.blade.php
@@ -0,0 +1,9 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>WaterHole</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+<html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <body>
+    <div class="container">
+        <div id="example"></div>
+    </div>
+  <script src="js/app.js"></script>
+</body>
+</html>
