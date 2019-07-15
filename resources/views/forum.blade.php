f20b142bcfb842d7655ccf0fa0acfd41f2dcbcb9
John Doe <xevoxe@gmail.com>
2019-07-15 14:05:48 -0500

    Refactor Project Structure

---
+++ resources/views/forum.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div id="categories">Categories</div>
+    </div>
+    <div class="col">
+        <div id="latesttopics">Latest Topics</div>
+    </div>
+  </div>
+</div>
+@endsection
--- resources/views/forumlayout.blade.php
+++
@@ -1,9 +0,0 @@
-<!Doctype html>
-<html>
-    <head>
-        <title>WaterHole</title>
-    </head>
-    <body>
-        @yield('content')
-    </body>
-<html>
